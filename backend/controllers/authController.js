8f9a6fd92e4afdf67087ea21c708ee2a93435582
Sar-thak83 <sarthak8385@gmail.com>
2025-08-07 00:03:16 +0530

    Initial commit

---
+++ backend/controllers/authController.js
@@ -0,0 +1,75 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+//generate Token
+const generateToken = (userId) => {
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
+};
+
+// register
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password, profileImageUrl } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already Exists" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      profileImageUrl,
+    });
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      profileImageUrl: user.profileImageUrl,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "server error ", error: error.message });
+  }
+};
+
+// login
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(500).json({ message: "User not exist ,Please Signup" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(500).json({ message: "Invalid Password" });
+    }
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      profileImageUrl: user.profileImageUrl,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "server error ", error: error.message });
+  }
+};
+
+//get user profile
+const getUserProfile = async (req, res) => {
+  try {
+    const user = await user.findById(req.user.id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not Found" });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: "server error ", error: error.message });
+  }
+};
+
+module.exports = { registerUser, loginUser, getUserProfile };
